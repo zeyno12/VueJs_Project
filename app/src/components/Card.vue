0344655daec4cb45a12ca0bea7f849270c50f4ec
zeyno12 <zeynabbagirova1@gmail.com>
2023-11-06 12:11:16 +0400

    create card and view detail page

---
+++ app/src/components/Card.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div class="card">
+      <div
+        class="card_box"
+        v-for="(user, index) in users.slice(0, 21)"
+        :key="user.id"
+        @click="goToDetailPage(user)"
+      >
+        <img :src="user.url" alt="" />
+        <p>{{ user.title }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      users: []
+    };
+  },
+  mounted() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/photos")
+      .then(response => {
+        this.users = response.data;
+      })
+      .catch(error => {
+        console.error("Oshibka pri poluchenii dannyx:", error);
+      });
+  },
+  methods: {
+    goToDetailPage(user) {
+      this.$router.push({
+        name: "Detail",
+        params: { userId: user.id },
+        query: { title: user.title, image: user.url }
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+.card_box {
+  border: 1px dashed red;
+  width: 300px;
+  height: 300px;
+  cursor: pointer;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  justify-content: center;
+  flex-direction: column;
+}
+.card_box img {
+  width: 80%;
+  height: 80%;
+  border-radius: 100%;
+  object-fit: cover;
+}
+.card_box p {
+  margin: 0;
+}
+</style>
---
+++ app/src/view/Detail.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="hello">
+      <h1>{{ msg }}</h1>
+    </div>
+    <div>
+      <h2>User Details</h2>
+      <p>User ID: {{ userId }}</p>
+      <p>User ID: {{ userTitle }}</p>
+      <img :src="userImage" alt="" />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
+        repellendus vel in iure fugit cupiditate porro voluptatum totam earum
+        id. A nemo deserunt veritatis earum similique incidunt doloremque
+        officiis, unde consequuntur, beatae voluptatem ratione? Doloremque
+        blanditiis ut officiis labore, ipsam laborum autem tempora? Cupiditate
+        inventore earum cum voluptatem voluptatibus quasi nemo libero quisquam
+        accusamus, nam suscipit beatae vero repudiandae explicabo velit in ab
+        dolor odio obcaecati magni rerum sit eos aliquam. Deserunt id omnis
+        perferendis eos distinctio hic quisquam laborum temporibus commodi.
+        Voluptatibus deserunt quis iste tempora modi necessitatibus libero
+        molestias vel reiciendis nemo sit non, facilis deleniti natus fugiat!
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Detail",
+  data() {
+    return {
+      msg: "salam askm about",
+      userId: null,
+      userTitle: null,
+      userImage:null
+    };
+  },
+  created() {
+    this.userId = this.$route.params.userId;
+    this.userTitle = this.$route.query.title;
+    this.userImage = this.$route.query.image;
+  }
+};
+</script>
+
+<style scoped></style>
